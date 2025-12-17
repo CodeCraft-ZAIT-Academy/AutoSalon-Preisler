1de74f04d6b2b4b763b8acc38acb5a02e0f250e2
jan preisler <jankpreisler@gmail.com>
2025-12-17 21:29:45 +0100

    Com

--- AutoSalon-Preisler/index.js
+++ AutoSalon-Preisler/index.js
@@ -1,2 +1,14 @@
+function rezervujAuto(nazov, cena, obrazok) {
+  const auto = {
+    nazov,
+    cena,
+    obrazok
+  };
 
+  let rezervacie = JSON.parse(localStorage.getItem('rezervacie')) || [];
+  rezervacie.push(auto);
+
+  localStorage.setItem('rezervacie', JSON.stringify(rezervacie));
+
+  window.location.href = "Mojerezervacie.html";
 }
