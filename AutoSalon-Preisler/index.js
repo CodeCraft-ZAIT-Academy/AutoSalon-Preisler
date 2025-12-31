52faf23693b5bde7a28f11d6ce99a494b623bd86
jan preisler <jankpreisler@gmail.com>
2025-12-31 14:55:16 +0100

    Filter_katalogu 1.0

--- AutoSalon-Preisler/index.js
+++ AutoSalon-Preisler/index.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Dosadenie dát do elementov v pop-upe
         document.querySelector('.popup-image').src = image;
         document.querySelector('.popup-text').textContent = name;
-         document.querySelector('.popup-prize').textContent = prize;
+        document.querySelector('.popup-prize').textContent = prize;
         document.querySelector('.popup-vykon').textContent = "Výkon: " + vykon;
         document.querySelector('.popup-rychlost').textContent = rychlost;
         document.querySelector('.infoska').textContent = info;
@@ -36,4 +36,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+window.filterCars = function(selectedBrand) {
+    // 1. Nájdeme všetky karty áut (uisti sa, že tvoje divy majú class="card")
+    const cards = document.querySelectorAll('.auto-card');
+
+    cards.forEach(card => {
+        
+        if (selectedBrand === 'all') {
+            card.style.display = "inline-block";
+        } 
+        // 3. Inak ukážeme len to, čo sa zhoduje, a ostatné skryjeme
+        else if (card.dataset.brand === selectedBrand) {
+            card.style.display = "inline-block";
+        } 
+        else {
+            card.style.display = "none";
+        }
+    });
+};
 
